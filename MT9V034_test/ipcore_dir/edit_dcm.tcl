72a12e264537dd47c5e4f749edc9af73937b975f
Georges Gauthier <glgauthier@wpi.edu>
2016-07-10 15:58:36 -0400

    camera reset, eagle cleanup

--- MT9V034_test/ipcore_dir/edit_dcm.tcl
+++ MT9V034_test/ipcore_dir/edit_dcm.tcl
@@ -22,7 +22,7 @@ proc findRtfPath { relativePath } {
 
 source [ findRtfPath "data/projnav/scripts/dpm_cgUtils.tcl" ]
 
-set result [ run_cg_edit "dcm" xc6slx16-3ftg256 Verilog ]
+set result [ run_cg_edit "dcm" xc6slx16-3csg324 Verilog ]
 
 if { $result == 0 } {
    puts "Core Generator edit command completed successfully."
--- MT9V034_test/mt9v034_top.v
+++ MT9V034_test/mt9v034_top.v
@@ -7,13 +7,14 @@
 module mt9v034_top(
 		input sysclk,
 		input reset,
+		input cam_rst,
 		input trigger,
 		output LOCKED,
 		output cam_sysclk,
+		output cam_reset,
 		output trigPin
     );
 
-wire trigDB;
 wire clk_50mS;
 wire clk_24MHz;
 
@@ -25,7 +26,22 @@ dcm CLK_24MHz
     .RESET(reset),
     .LOCKED(LOCKED)
 	 ); 
-	 
+
+ODDR2 #(
+      .DDR_ALIGNMENT("NONE"), // Sets output alignment to "NONE", "C0" or "C1" 
+      .INIT(1'b0),    // Sets initial state of the Q output to 1'b0 or 1'b1
+      .SRTYPE("SYNC") // Specifies "SYNC" or "ASYNC" set/reset
+   ) clock_forward_inst (
+      .Q(cam_sysclk),     // 1-bit DDR output data
+      .C0(clk_24MHz),  // 1-bit clock input
+      .C1(~clk_24MHz), // 1-bit clock input
+      .CE(1'b1),      // 1-bit clock enable input
+      .D0(1'b0), // 1-bit data input (associated with C0)
+      .D1(1'b1), // 1-bit data input (associated with C1)
+      .R(1'b0),   // 1-bit reset input
+      .S(1'b0)   // 1-bit set input
+   );
+	
 clk_div debounce_clk(
 	 .reset(reset), // synchronous reset
     .clk_24M(clk_24MHz), // 24MHz clock signal
@@ -36,10 +52,14 @@ clk_div debounce_clk(
 debounce deb(
     .clk(clk_50mS),
     .btn(trigger),
-    .btn_val(trig_DB)
+    .btn_val(trigPin)
     );
-
-assign trigPin = trig_DB;
-assign cam_sysclk = clk_24MHz;
+	 
+// debounce camera reset button
+debounce deb1(
+    .clk(clk_50mS),
+    .btn(cam_rst),
+    .btn_val(cam_reset)
+    );
 
 endmodule
