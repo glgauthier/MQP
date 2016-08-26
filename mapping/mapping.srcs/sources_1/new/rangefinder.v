ac9b601157b7f1d592bbb04a5660689b3226f382
jadecusati <jadecusati@gmail.com>
2016-08-26 14:39:38 -0400

    switched to vivado to start coding on zedboard

---
+++ mapping/mapping.srcs/sources_1/new/rangefinder.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 08/26/2016 02:35:36 PM
+// Design Name: 
+// Module Name: rangefinder
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module rangefinder(
+
+    );
+endmodule
